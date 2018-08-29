94e52a228d97c4d0e1dc8a29378eb9a648ba6157
regtimes <regtimes.s@gmail.com>
2018-08-29 20:02:18 +0300

    Создаем конструктор

--- Dragon/Dragon/Point.cs
+++ Dragon/Dragon/Point.cs
@@ -12,10 +12,17 @@ namespace Dragon
         public int y;
         public char sym;
 
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
-        }
+         }
     }
 }
